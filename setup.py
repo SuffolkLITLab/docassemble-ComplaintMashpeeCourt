ee3ae4cb8bc5475a86f8f50ae58f11907263a4cc
Anonymous User <106634366+ivetteacuenod@users.noreply.github.com>
2022-08-19 11:08:20 -0400

    Commit defendats ifelse

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ from distutils.util import convert_path
 
 standard_exclude = ('*.pyc', '*~', '.*', '*.bak', '*.swp*')
 standard_exclude_directories = ('.*', 'CVS', '_darcs', './build', './dist', 'EGG-INFO', '*.egg-info')
+
 def find_package_data(where='.', package='', exclude=standard_exclude, exclude_directories=standard_exclude_directories):
     out = {}
     stack = [(convert_path(where), '', package)]
@@ -53,7 +54,7 @@ setup(name='docassemble.ComplaintMashpeeCourt',
       url='https://courtformsonline.org',
       packages=find_packages(),
       namespace_packages=['docassemble'],
-      install_requires=['docassemble.AssemblyLine>=2.13.0'],
+      install_requires=['docassemble.AssemblyLine>=2.14.1'],
       zip_safe=False,
       package_data=find_package_data(where='docassemble/ComplaintMashpeeCourt/', package='docassemble.ComplaintMashpeeCourt'),
      )
