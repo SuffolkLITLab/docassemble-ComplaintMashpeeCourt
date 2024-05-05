2ba93f388586197942dcfefc5601242125134f91
Christopher Emrich <133987500+CWEmrich@users.noreply.github.com>
2024-05-05 12:25:50 -0400

    Final Commit

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ setup(name='docassemble.ComplaintMashpeeCourt',
       url='https://courtformsonline.org',
       packages=find_packages(),
       namespace_packages=['docassemble'],
-      install_requires=['docassemble.AssemblyLine>=2.27.2'],
+      install_requires=['docassemble.AssemblyLine>=2.28.1'],
       zip_safe=False,
       package_data=find_package_data(where='docassemble/ComplaintMashpeeCourt/', package='docassemble.ComplaintMashpeeCourt'),
      )
